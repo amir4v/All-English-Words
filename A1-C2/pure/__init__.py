62f43dd1f377098ef9249500ee6fb8460da04515
Amir <84547283+amir4v@users.noreply.github.com>
2023-12-14 22:48:39 +0330

    Update __init__.py

--- A1-C2/pure/__init__.py
+++ A1-C2/pure/__init__.py
@@ -1,17 +1,18 @@
 #TODO:
 """
 - definition of each word of A1-C2 words with synonym and antonym
-	https://www.ldoceonline.com/dictionary/WORD
+	* https://www.ldoceonline.com/dictionary/WORD
+	* https://dictionary.langeek.co/
 	https://www.oxfordlearnersdictionaries.com/definition/english/WORD
 	https://www.dictionary.com/browse/WORD
- 	-
-  	https://www.englisch-hilfen.de/en/words_inhalt.htm
+	-
+	https://www.englisch-hilfen.de/en/words_inhalt.htm
 	https://www.englisch-hilfen.de/en/words_list/alle.htm
 - detailed, to the end, of grammars, nested, related
 	https://learnenglish.britishcouncil.org/grammar
- 	https://www.grammarly.com/grammar#sectionGroup_6iKEWxDNd9Glgyj522RuVP
-  	https://langeek.co/en/grammar
-	https://www.grammarinlevels.com/
- 	https://www.englisch-hilfen.de/en/inhalt_grammar.htm
+	https://www.grammarly.com/grammar#sectionGroup_6iKEWxDNd9Glgyj522RuVP
+	https://langeek.co/en/grammar
+	https://www.grammarinlevels.com
+	https://www.englisch-hilfen.de/en/inhalt_grammar.htm
 - [chatGPT talk about this word or sentence in three lines] for collected topics
 """
